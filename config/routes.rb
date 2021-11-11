e0806d22ed60009c73316b259823c2be8836f909
AndreaJasper <andrea.nicole.jasper@gmail.com>
2021-11-11 16:40:10 -0700

    added about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#welcome'
 
   get '/portfolio', to: 'portfolio#portfolio', as: 'portfolio'
+  get '/about', to: 'about#about', as: 'about'
+  get '/contact', to: 'contact#contact', as: 'contact'
 end
